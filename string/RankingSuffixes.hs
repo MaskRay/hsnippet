13ac378cff6f9f4331105cebd3d8aa91d1b1916e
MaskRay <emacsray@gmail.com>
2012-01-08 18:38:29 +0800

    * string/Rankingsuffixes O(n * log n) (radix sort)

--- string/RankingSuffixes.hs
+++ string/RankingSuffixes.hs
@@ -1,6 +1,7 @@
 module RankingSuffixes (
   rank, rankTails1
   ) where
+
 import Control.Applicative
 import Control.Arrow
 import Control.Monad
@@ -8,14 +9,32 @@ import Control.Monad.Instances
 import Data.Array
 import Data.Function
 import Data.List
+import Control.Monad.ST
+import Data.Array.ST
 
 rank :: (Ord a) => [a] -> [Int]
 rank = elems . liftM2 array ((,) 0 . pred . length) (map (first snd) . concat . label . groupBy ((==) `on` fst) . sort . flip zip [0..])
   where
     label = zipWith (flip (map . flip (,))) <*> scanl (+) 0 . map length
 
+boundRank :: [(Int,Int)] -> [Int]
+boundRank xs = elems . array (0,n-1) . map (first snd) . concat . label . groupBy ((==) `on` fst) . countingSortBy (fst.fst) n . countingSortBy (succ.snd.fst) (n+1) $ xs `zip` [0..]
+  where
+    n = length xs
+    label = zipWith (flip (map . flip (,))) <*> scanl (+) 0 . map length
+
 rankTails1 :: (Ord a) => [a] -> [Int]
 rankTails1 = liftM3 applyUntil (((and . elems).) . (.flip zip (repeat True)) . accumArray (||) False . (,) 0 . pred . length) (const $ map reorder (iterate (*2) 1)) rank
   where
-    reorder = (rank.) . (zip <*>) . ((++ repeat (-1)).) . drop
+    reorder = (boundRank.) . (zip <*>) . ((++ repeat (-1)).) . drop
     applyUntil p fs x = head . dropWhile (not . p) $ scanl (flip ($)) x fs
+
+countingSortBy :: (a -> Int) -> Int -> [a] -> [a]
+countingSortBy f n xs = runST $ do
+  a <- newArray (0,n-1) 0 :: ST s (STUArray s Int Int)
+  forM_ xs $ \x -> readArray a (f x) >>= writeArray a (f x) . succ
+  forM_ [1..n-1] $ \i -> readArray a (i-1) >>= \p -> readArray a i >>= \q -> writeArray a i (p+q)
+  (elems . array (0,length xs-1)) `fmap` forM (reverse xs) (\x -> do
+    i <- pred `fmap` readArray a (f x)
+    writeArray a (f x) i
+    return (i,x))
